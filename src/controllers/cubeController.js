77d8a5d03121efc5768273301096380d5d2df985
Medi-Demirev <93528758+Medi-Demirev@users.noreply.github.com>
2022-06-06 01:22:51 +0300

    Add static details page

--- src/controllers/cubeController.js
+++ src/controllers/cubeController.js
@@ -1,7 +1,5 @@
 const router = require('express').Router();
-const fs = require('fs/promises');
-const path = require('path');
-const cubes = require("../db.json");
+const cubeService = require('../services/cubeService');
 
 router.get('/create', (req, res)=>{
     res.render('create');
@@ -15,14 +13,16 @@ router.post('/create', (req, res)=>{
         return res.status(400).send('Invalid request');
     }
 
-    cubes.push(cube);
-    fs.writeFile(path.resolve('src','db.json'), JSON.stringify(cubes,'', 4), {encoding: 'utf-8'})
+        cubeService.save(cube)
         .then(()=>{
             res.redirect('/')
         })
         .catch(err =>{
             res.status(40).send(err)
-        })
+        })     
 });
 
+router.get('/details/:id', (req, res)=>{
+    res.render('details')
+})
 module.exports = router;
