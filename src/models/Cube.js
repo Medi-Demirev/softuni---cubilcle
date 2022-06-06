b1b1fade28d04e30c219b789d04353635545cc6e
Medi-Demirev <93528758+Medi-Demirev@users.noreply.github.com>
2022-06-06 14:46:03 +0300

    Create cube model

---
+++ src/models/Cube.js
@@ -0,0 +1,32 @@
+const mongoose = require('mongoose'); 
+
+const cubeSchema = new mongoose.Schema({
+    _id: mongoose.Types.ObjectId,
+    name: {
+        type: String,
+        required: true,
+    },
+    descryption:{
+        type:String,
+        required: true,
+        maxlength: 120,
+    },
+    imageUrl:{
+        type:String,
+        required: true,
+    },
+    difficultyLevel:{
+        type: Number,
+        required: true,
+        min: 1,
+        max: 6,
+    }
+});
+
+cubeSchema.path('iamgeUrl').validate(function () {
+    return this.name.startsWith('http', 'image url should be a link' )
+});
+
+const Cube = mongoose.model('Cube', cubeSchema);
+
+module.exports = Cube;
